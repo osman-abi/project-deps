ec15e44b355c6a564ac48b349fa826fb39075add
osman-abi <osman.97.abi@gmail.com>
2022-06-19 13:34:17 +0400

    status updated

--- core/core/settings.py
+++ core/core/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7b^@#tt9nhbr2l_^qgr_nlvo)3#b3(#jej^c*39!j@v_wjs*pa'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
