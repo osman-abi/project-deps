523a70fedebc3b258a7dea6c597127d79033a75b
osman-abi <osman.97.abi@gmail.com>
2022-06-20 11:03:20 +0400

    status updated

--- core/core/settings.py
+++ core/core/settings.py
@@ -177,7 +177,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-if DEBUG:
+if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, "static")
 else:
     STATICFILES_DIRS = [
