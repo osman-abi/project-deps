2bd354ce86f74078b828cf0734bcc3c16b0da48f
osman-abi <osman.97.abi@gmail.com>
2022-06-20 10:55:36 +0400

    status updated

--- core/core/settings.py
+++ core/core/settings.py
@@ -127,7 +127,7 @@ if not DEBUG:
             'USER': 'deps_db_user',
             'NAME': 'deps_db',
             'PASSWORD': 'deps_db_123',
-            'HOST': 'postgres',
+            'HOST': 'localhost',
             'PORT': 5432,
         }
     }
