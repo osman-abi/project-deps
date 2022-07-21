135186e3c6e3032f630b342295c2f23bb223a34c
osman-abi <osman.97.abi@gmail.com>
2022-07-21 20:28:47 +0400

    local

--- core/shop/models.py
+++ core/shop/models.py
@@ -75,7 +75,7 @@ class Product(models.Model):
     is_featured = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.title
+        return self.id
 
     def save(self):
         import string
